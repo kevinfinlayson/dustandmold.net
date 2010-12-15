63a7ec1cd1cd655fd88eb5678d752978fa5ef518
Kevin Finlayson <kevin@dustandmold.net>
2010-12-15 04:13:17 -0500

    built out dark version

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 
 $(document).ready(function(){
   $(".slide, figcaption").hide()
-  $("#slide-info").after('<div id="prevnext"><a href="#" title="Previous" id="previous">Previous</a><a href="#" title="Next" id="next">Next</a></div><!--! prevnext -->');
+  $("#slideshow-container").prepend('<a href="#" title="Previous" id="previous">Previous</a><a href="#" title="Next" id="next">Next</a>');
   $("#slideshow").cycle({
     timeout: 0,
     speed: "fast",
