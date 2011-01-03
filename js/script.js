bb0adf2b9204c9e99e0b5bbaa4950c5e4a32ce44
Kevin Finlayson <kevin@dustandmold.net>
2011-01-03 09:41:50 -0500

    tweaking the slideshow

--- js/script.js
+++ js/script.js
@@ -2,17 +2,31 @@
 */
 
 $(document).ready(function(){
-  $("#slideshow-container").prepend('<a href="#" id="previous"><span>Previous</span></a><a href="#" id="next"><span>Next</span></a>');  // Add the previous and next links
-  $("#previous, #next").css({"opacity":"0"}) // Hide the previous and next links
-  $("#slideshow").cycle({ // Cycle
+  $("#slideshow-container").prepend('<a href="#" id="previous"><span>Previous</span></a><a href="#" id="next"><span>Next</span></a>');
+  $("#slideshow-container").append('<div id="slide-info"><div id="slide-info-content"></div><!--! end of #slide-info-content --></div><!--! end of #slide-info -->');
+  $("#slideshow").cycle({
     timeout: 0,
-    speed: "fast",
+    speed: 300,
     next: "#next",
     prev: "#previous",
+    before: onBefore,
+    after: onAfter
   });
-  $("#slideshow .slide:first-child").children("img").imagesLoaded(function(){ // Wait for first slide image to load
+  function onBefore(){
+    $("#slide-info-content").fadeOut(300);
+  }
+  function onAfter(){
+    console.log(this);
+    var slideInfo = $(this).children(".slide-info").html();
+    console.log(slideInfo);
+    $("#slide-info-content").html(slideInfo);
+    $("#slide-info-content").fadeIn(300);
+  }
+  $("#slideshow .slide:first-child").children("img").imagesLoaded(function(){
+    var slideInfo = $(this).siblings(".slide-info").html();
+    $("#slide-info-content").html(slideInfo);
     $("#slideshow").animate({opacity:1}, 500, function(){
-      $("#previous, #next").animate({opacity:1},300);
+      $("#previous, #next, #slide-info-content").animate({opacity:1},300);
     });
   });
 })
