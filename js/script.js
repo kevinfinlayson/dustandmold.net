a71ffd290d5a06d2ce5c800cfafea63425e149c2
Kevin Finlayson <kevin@dustandmold.net>
2011-02-01 01:12:27 -0500

    finishing up everything I can manage

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   // Add the slideshow navigation and info elements
   $("#slideshow-container").prepend('<a href="#-do" id="top-close"><span>Close</span></a><div id="pager"></div><!--! end of #pager --><a href="#" id="previous"><span>Previous</span></a><a href="#" id="next"><span>Next</span></a>');
   $("#slideshow-container").append('<div id="slide-info"><div id="slide-info-content"></div><!--! end of #slide-info-content --></div><!--! end of #slide-info -->');
-  $(".slide").prepend('<img src="graphics/slide-overlay.png" alt="slide overlay graphic" class="slide-overlay" />');
+  $(".slide").prepend('<div class="slide-overlay"></div><!--! end of .slide-overlay -->');
   $("#slideshow").cycle({
     timeout: 0,
     speed: 100,
@@ -17,9 +17,18 @@ $(document).ready(function(){
     pagerAnchorBuilder: function(idx, slide) {
       var slideTitle = $(".slide-info h3", slide).html();
       var slideImgSrc = $("img:nth-child(2)", slide).attr("src");
-      return '<a href="#" class="thumb" title="' + slideTitle + '"><img src="graphics/thumb-overlay.png" alt="thumb overlay graphic" class="thumb-overlay" /><img src="' + slideImgSrc + '" alt="' + slideTitle + ' image" /><h3>' + slideTitle + '</h3></a>';
+      return '<a href="#" class="thumb" title="' + slideTitle + '"><div class="thumb-overlay"></div><!--! end of .thumb-overlay --><img src="' + slideImgSrc + '" alt="' + slideTitle + ' image" /><h3>' + slideTitle + '</h3></a>';
     }
   });
+  function onBefore(){
+    $("#slide-info-content").fadeOut(100);
+  }
+  function onAfter(){
+    var slideInfo = $(this).children(".slide-info").html();
+    $("#slide-info-content").html(slideInfo);
+    $("#slide-info-content").fadeIn(100);
+  }
+
   // Switch from pager to slideshow
   $("#pager a").click(function(){
     $("#pager").fadeOut(100, function(){
@@ -38,35 +47,21 @@ $(document).ready(function(){
       $("#pager").show().animate({opacity:1},100);
     })
   })
-  function onBefore(){
-    $("#slide-info-content").fadeOut(300);
-  }
-  function onAfter(){
-    var slideInfo = $(this).children(".slide-info").html();
-    $("#slide-info-content").html(slideInfo);
-    $("#slide-info-content").fadeIn(300);
-  }
   function onResize() {
     var windowWidth = $(window).width();
-    if (windowWidth >= 1020) {
-      $(".thumb-overlay").attr('src', 'graphics/thumb-overlay.png');
-      $(".slide-overlay").attr('src', 'graphics/slide-overlay.png');
+    if (windowWidth > 1024) {
       $("#slideshow-container").addClass('thumb-display');
       $("#slideshow, #previous, #next, #slide-info, #slide-info-content, #top-close, .slide-overlay").css({"opacity":"0"}).hide();
       $("#pager").show().css({"opacity":"1"});
-     } else if (windowWidth < 1020 && windowWidth >= 768 ) {
-      $(".thumb-overlay").attr('src', 'graphics/thumb-overlay-ipad.png');
-      $(".slide-overlay").attr('src', 'graphics/slide-overlay-ipad.png');
+     } else if (windowWidth < 1020 && windowWidth > 768 ) {
       $("#slideshow-container").addClass('thumb-display');
       $("#slideshow, #previous, #next, #slide-info, #slide-info-content, #top-close, .slide-overlay").css({"opacity":"0"}).hide();
       $("#pager").show().css({"opacity":"1"});
     }  else if (windowWidth < 768 && windowWidth >= 480) {
-      $(".slide-overlay").attr('src', 'graphics/slide-overlay-iphone-landscape.png');
       $("#slideshow-container").removeClass('thumb-display');
       $("#slideshow, #previous, #next, #slide-info, #slide-info-content, .slide-overlay").css({"opacity":"1"}).show();
       $("#pager").css({"opacity":"0"}).hide();
     } else if (windowWidth < 480 && windowWidth >= 320) {
-      $(".slide-overlay").attr('src', 'graphics/slide-overlay-iphone.png');
       $("#slideshow-container").removeClass('thumb-display');
       $("#slideshow, #previous, #next, #slide-info, #slide-info-content, .slide-overlay").css({"opacity":"1"}).show();
       $("#pager").css({"opacity":"0"}).hide();
@@ -79,11 +74,12 @@ $(document).ready(function(){
       $("#pager").show().animate({opacity:1},100);
     } else {
       $("#pager").css({"opacity":"0"}).hide();
-      $("#slideshow").show().animate({opacity:1},100);
+      $("#slideshow, #previous, #next, #slide-info, #slide-info-content, .slide-overlay").show().animate({opacity:1},100);
     }
   });
   $(window).resize(function(){
-    console.log("resized!");
+    var width = $(window).width();
+    console.log(width);
     onResize();
   });
 })
